0a6d1abdefbd211d41cef27a4a67dcf3a128710d
Gopi Rayini <khanna12@bu.edu>
2025-09-09 12:44:54 -0400

    Added Spring Initializr Code

---
+++ backend/src/test/java/com/cs673/CurseForge/CurseForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs673.CurseForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
