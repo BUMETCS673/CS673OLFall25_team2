f18ba4bfa34b8ca321d3f28194c66f869fdee4f1
Gopi Rayini <khanna12@bu.edu>
2025-09-11 21:54:32 -0400

    Hello World api setup

--- code/backend/src/main/java/com/cs673/CurseForge/CurseForgeApplication.java
+++ code/backend/src/main/java/com/cs673/CurseForge/CurseForgeApplication.java
@@ -2,12 +2,12 @@ package com.cs673.CurseForge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class CurseForgeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurseForgeApplication.class, args);
 	}
-
 }
