53cbe15702be3e8c03a626879d3d0c2b40cea6b0
gopi-k-rayini <khanna12@bu.edu>
2025-09-14 19:05:25 -0400

    Update CareerForgeApplication.java

--- code/backend/src/main/java/com/cs673/CareerForge/CareerForgeApplication.java
+++ code/backend/src/main/java/com/cs673/CareerForge/CareerForgeApplication.java
@@ -1,13 +1,13 @@
-package com.cs673.CareerForge;
+package com.cs673.careerforge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class CareerForgeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CareerForgeApplication.class, args);
 	}
+
 }
