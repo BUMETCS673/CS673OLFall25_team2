025128847bea6c9e0c754fabdbaa31ec1723ea71
rose191919 <jamesrose1902@gmail.com>
2025-09-15 00:19:17 -0400

    Added RegisterForm and RegisterForm test

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -1,16 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main';
-import MyJobs from './pages/MyJobs';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import MyJobs from "./pages/MyJobs";
+import LoginForm from "./components/loginAndRegistration/LoginForm";
+import RegisterForm from "./components/loginAndRegistration/RegisterForm";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/my-jobs" element={<MyJobs />} />
+        <Route path="/login" element={<LoginForm showSubmitButton />} />
+        <Route path="/register" element={<RegisterForm showSubmitButton />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
