2c5f54f2e8c0217e18116bd61fceed3512c4d0de
Pedro Ramirez <prlara23@bu.com>
2025-09-16 22:32:59 -0500

    add folders and files

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginForm from './components/loginAndRegistration/LoginForm';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <LoginForm />
       <Routes>
         <Route path="/myJobs" element={<div>My Jobs Page</div>} />
         <Route path="/" element={<div>Main Page</div>} />
