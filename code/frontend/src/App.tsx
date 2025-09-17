1fc4974c5fda19050ac2b6c7a687a42b4df32631
Pedro Ramirez <prlara23@bu.com>
2025-09-16 22:42:10 -0500

    fix error

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LoginForm from './components/loginAndRegistration/LoginForm';
-import './index.css';
+import './styles/global.css';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <LoginForm />
       <Routes>
         <Route path="/myJobs" element={<div>My Jobs Page</div>} />
         <Route path="/" element={<div>Main Page</div>} />
