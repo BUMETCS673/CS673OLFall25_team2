3de286435481c5c31b4b287e9ec9505e68821a25
GeorgiaM66 <szdyesheng1221@gmail.com>
2025-09-19 16:17:10 -0400

    [29] modified endpoints for jobs API.

--- code/backend/src/main/java/com/cs673/careerforge/web/JobController.java
+++ code/backend/src/main/java/com/cs673/careerforge/web/JobController.java
@@ -2,33 +2,34 @@ package com.cs673.careerforge.web;
 
 import com.cs673.careerforge.request.JobRequest;
 import com.cs673.careerforge.response.ListJobResponse;
-import com.cs673.careerforge.vo.JobVO;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class JobController {
 
-    @GetMapping("/jobs/{id}")
-    public JobVO getJob(@PathVariable Long id, HttpServletRequest httpServletRequest) {
-        return JobVO.builder().id(id).title("test title").location("test location").build();
-    }
-
-    @PostMapping("/jobs/save")
-    public Boolean saveJob(@RequestBody JobRequest request, HttpServletRequest httpServletRequest) {
-        return true;
-    }
-
-    @PostMapping("/jobs/apply")
-    public Boolean applyJob(@RequestBody JobRequest request, HttpServletRequest httpServletRequest) {
-        return true;
-    }
-
-    @PostMapping("/jobs/all")
+    @GetMapping("/jobs")
     public ListJobResponse listJob(HttpServletRequest httpServletRequest) {
         return ListJobResponse.builder().build();
+    }
+
+    @PostMapping("/jobs/{id}/save")
+    public Boolean saveJob(@PathVariable Long id, @RequestBody JobRequest request, HttpServletRequest httpServletRequest) {
+        return true;
+    }
+
+    @PostMapping("/jobs/{id}/apply")
+    public Boolean applyJob(@PathVariable Long id, @RequestBody JobRequest request, HttpServletRequest httpServletRequest) {
+        return true;
+    }
+
+    @DeleteMapping("/jobs/{id}")
+    public Boolean deleteJob(@RequestBody JobRequest request, HttpServletRequest httpServletRequest) {
+        return true;
     }
 }
